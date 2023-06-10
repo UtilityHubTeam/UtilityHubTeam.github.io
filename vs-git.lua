254742e0c0f71e7ce07b3652a93f9da634d7c5fa
UtilityHub <106483689+UtilityHub@users.noreply.github.com>
2023-06-09 22:10:34 -0400

    Update vs-git.lua

--- vs-git.lua
+++ vs-git.lua
@@ -1,11 +1,4 @@
---[[
-██╗░░░██╗████████╗██╗██╗░░░░░██╗████████╗██╗░░░██╗██╗░░██╗██╗░░░██╗██████╗░  ██╗░░██╗
-██║░░░██║╚══██╔══╝██║██║░░░░░██║╚══██╔══╝╚██╗░██╔╝██║░░██║██║░░░██║██╔══██╗  ╚██╗██╔╝
-██║░░░██║░░░██║░░░██║██║░░░░░██║░░░██║░░░░╚████╔╝░███████║██║░░░██║██████╦╝  ░╚███╔╝░
-██║░░░██║░░░██║░░░██║██║░░░░░██║░░░██║░░░░░╚██╔╝░░██╔══██║██║░░░██║██╔══██╗  ░██╔██╗░
-╚██████╔╝░░░██║░░░██║███████╗██║░░░██║░░░░░░██║░░░██║░░██║╚██████╔╝██████╦╝  ██╔╝╚██╗
-░╚═════╝░░░░╚═╝░░░╚═╝╚══════╝╚═╝░░░╚═╝░░░░░░╚═╝░░░╚═╝░░╚═╝░╚═════╝░╚═════╝░  ╚═╝░░╚═╝
-]]
+--ＵｔｉｌｉｔｙＨｕｂ－ Ｎ －ＰＬＡＹ
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))();       
 
@@ -14,22 +7,12 @@ local games = {
     ["Anime Souls Simulator"] = 11542692507,
 }
 
-loadstring(game:HttpGet("https://www.utilityhub.gq/execution.lua"))();   
+loadstring(game:HttpGet("https://www.utilityhub.gq/execution.lua"))();
 
-local http = game:GetService('HttpService')
-local req = (syn and syn.request) or (http and http.request) or http_request
-if req then
-req({
-Url = 'http://127.0.0.1:6463/rpc?v=1',
-Method = 'POST',
-Headers = {
-['Content-Type'] = 'application/json',
-Origin = 'https://discord.com'
-},
-Body = http:JSONEncode({
-cmd = 'INVITE_BROWSER',
-nonce = http:GenerateGUID(false),
-args = {code = 'xejgTD9Wpj'}
-})
-})
-end
+local get keys = {
+    ["input key"] "",
+}
+
+   
+-- Credits: Sand Dog#5240 / ! Sunn#5342
+-- Server Link: https://discord.com/invite/xejgTD9Wpj
