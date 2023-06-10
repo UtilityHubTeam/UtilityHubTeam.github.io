916d1b65dc98489c5c422b0776e7e57bad5b67d5
UtilityHub <106483689+UtilityHub@users.noreply.github.com>
2023-06-09 22:30:18 -0400

    Update vs-git.lua

--- vs-git.lua
+++ vs-git.lua
@@ -1,18 +1,15 @@
 --ＵｔｉｌｉｔｙＨｕｂ－ Ｎ －ＰＬＡＹ
+-- Credits: Sand Dog#5240 / ! Sunn#5342
+-- Server Link: https://discord.com/invite/xejgTD9Wpj
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))();       
 
-local games = {
+local supportivegames = {
     ["Anime Rising Figher Simulator"] = 11978447465,
     ["Anime Souls Simulator"] = 11542692507,
 }
 
-local keysystem = {
-    ["input key"] "",
+local utilitykey = {
+    ["Get key...!"] = "",
 }
-
 loadstring(game:HttpGet("https://www.utilityhub.gq/execution.lua"))();
-end
-   
--- Credits: Sand Dog#5240 / ! Sunn#5342
--- Server Link: https://discord.com/invite/xejgTD9Wpj
